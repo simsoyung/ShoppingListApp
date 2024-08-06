bfbacec74ce0cf61f61bfe0a91de788fb070af65
soyung sim <simsy1024@gmail.com>
2024-08-06 17:22:26 +0900

    Rx

--- ShoppingListApp/ShoppingTableViewCell.swift
+++ ShoppingListApp/ShoppingTableViewCell.swift
@@ -6,11 +6,72 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
 class ShoppingTableViewCell: UITableViewCell {
-
-    @IBOutlet var checkButton: UIButton!
-    @IBOutlet var starButton: UIButton!
-    @IBOutlet var mainLabel: UILabel!
+    
+    
+    static let identifier = "ShoppingTableViewCell"
+    var disposeBag = DisposeBag()
+    let todoLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
+    let checkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .black
+        return imageView
+    }()
+    let likeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .black
+        return imageView
+    }()
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.backgroundColor = .systemGray6
+        self.backgroundColor = .clear
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        contentView.addSubview(todoLabel)
+        contentView.addSubview(checkImageView)
+        contentView.addSubview(likeImageView)
+        
+        checkImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(12)
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(20)
+            make.size.equalTo(25)
+        }
+        todoLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(checkImageView)
+            make.leading.equalTo(checkImageView.snp.trailing).offset(20)
+            make.height.equalTo(30)
+        }
+        likeImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(10)
+            make.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(20)
+            make.size.equalTo(25)
+        }
+    }
     
 }
--- ShoppingListApp/ShoppingTableViewController.swift
+++ ShoppingListApp/ShoppingTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 struct ShoppingList: Hashable, Identifiable {
     let id = UUID()
@@ -25,48 +27,97 @@ class ShoppingTableViewController: UITableViewController {
     @IBOutlet var textField: UITextField!
     @IBOutlet var addbutton: UIButton!
     @IBOutlet var buttonView: UIView!
-    
+    let disposeBag = DisposeBag()
     lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
     func createLayout() -> UICollectionViewLayout {
         var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
-        configuration.backgroundColor = .gray
+        configuration.backgroundColor = .systemGray6
         configuration.showsSeparators = false
         let layout = UICollectionViewCompositionalLayout.list(using: configuration)
         return layout
     }
+    lazy var list = BehaviorSubject(value: checkList)
     
     var dataSource: UICollectionViewDiffableDataSource<Section, ShoppingList>!
     
     var checkList = [
-    ShoppingList(star: true, check: false, textLabel: "그립톡 구매하기"),
-    ShoppingList(star: true, check: false, textLabel: "사이다 구매"),
-    ShoppingList(star: false, check: true, textLabel: "양말"),
-    ShoppingList(star: true, check: true, textLabel: "양배추"),
-    ShoppingList(star: false, check: false, textLabel: "아이패드 케이스"),
-    ShoppingList(star: true, check: true, textLabel: "자전거"),
-    ShoppingList(star: false, check: false, textLabel: "노트"),
-    ShoppingList(star: false, check: true, textLabel: "세탁세제"),
-    ShoppingList(star: true, check: true, textLabel: "면봉"),
+        ShoppingList(star: true, check: false, textLabel: "그립톡 구매하기"),
+        ShoppingList(star: true, check: false, textLabel: "사이다 구매"),
+        ShoppingList(star: false, check: true, textLabel: "양말"),
+        ShoppingList(star: true, check: true, textLabel: "양배추"),
+        ShoppingList(star: false, check: false, textLabel: "아이패드 케이스"),
+        ShoppingList(star: true, check: true, textLabel: "자전거"),
+        ShoppingList(star: false, check: false, textLabel: "노트"),
+        ShoppingList(star: false, check: true, textLabel: "세탁세제"),
+        ShoppingList(star: true, check: true, textLabel: "면봉"),
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
         textField.placeholder = "무엇을 구매하실 건가요?"
-        textField.font = .boldSystemFont(ofSize: 13)
+        textField.font = .boldSystemFont(ofSize: 14)
         textField.layer.cornerRadius = 10
         textField.textColor = .black
-        textField.backgroundColor = .systemGray5
+        textField.backgroundColor = .systemGray6
         
         addbutton.titleLabel?.text = "추가"
         addbutton.backgroundColor = .systemGray4
         addbutton.tintColor = .black
         addbutton.layer.cornerRadius = 10
-        buttonView.backgroundColor = .systemGray5
+        buttonView.backgroundColor = .systemGray6
         tableView.rowHeight = 50
         addbutton.addTarget(self, action: #selector(addButtonClicked), for: .touchUpInside)
         configurationCell()
+        bind()
+    }
+    
+    func bind(){
+        list
+            .bind(to: tableView.rx.items(cellIdentifier: ShoppingTableViewCell.identifier, cellType: ShoppingTableViewCell.self)) { (row, element, cell) in
+                let check = element.check ? "checkmark.square.fill" : "checkmark.square"
+                cell.checkImageView.rx.image = UIImage(systemName: check)
+                
+                let star = element.star ? "star.fill" : "star"
+                let starImage = UIImage(systemName: star)
+                cell.starButton.setImage(starImage, for: .normal)
+                
+                cell.checkButton.rx.tap
+                    .subscribe(onNext: { [weak self] in
+                        self?.checkList[row].check.toggle()
+                        self?.list.onNext(self?.checkList ?? [])
+                    })
+                    .disposed(by: cell.disposeBag)
+                
+                cell.starButton.rx.tap
+                    .subscribe(onNext: { [weak self] in
+                        self?.checkList[row].star.toggle()
+                        self?.list.onNext(self?.checkList ?? [])
+                    })
+                    .disposed(by: cell.disposeBag)
+            }
+            .disposed(by: disposeBag)
         
+        tableView.rx.itemDeleted
+            .subscribe(onNext: { [weak self] indexPath in
+                self?.checkList.remove(at: indexPath.row)
+                self?.list.onNext(self?.checkList ?? [])
+            })
+            .disposed(by: disposeBag)
+        
+        addbutton.rx.tap
+            .subscribe(onNext: { value in
+                guard let text = self.textField.text, text.count > 1 else {
+                    self.textField.placeholder = "2글자 이상 입력해주세요"
+                    return
+                }
+                let newItem = ShoppingList(star: false, check: false, textLabel: text)
+                self.checkList.append(newItem)
+                
+                self.textField.text = ""
+                self.list.onNext(self.checkList)
+            })
+            .disposed(by: disposeBag)
     }
     
     func updateData(){
@@ -90,7 +141,7 @@ class ShoppingTableViewController: UITableViewController {
             cell.contentConfiguration = content
             
             var background = UIBackgroundConfiguration.listPlainCell()
-            background.backgroundColor = .gray
+            background.backgroundColor = .systemGray6
             background.cornerRadius = 10
             background.strokeWidth = 1
             background.strokeColor = .systemBlue
@@ -100,7 +151,7 @@ class ShoppingTableViewController: UITableViewController {
             let cell = collectionView.dequeueConfiguredReusableCell(using: registeration, for: indexPath, item: itemIdentifier)
             return cell
         })
-    
+        
     }
     
     @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
@@ -130,21 +181,17 @@ class ShoppingTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return checkList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell") as! ShoppingTableViewCell
         
         let date = checkList[indexPath.row]
-        cell.mainLabel.text = date.textLabel
-        cell.mainLabel.font = UIFont.systemFont(ofSize: 12)
-        cell.mainLabel.textColor = .black
-        cell.mainLabel.textAlignment = .left
-        cell.backgroundColor = .systemGray5
+        cell.backgroundColor = .systemGray6
         cell.contentView.frame.inset(by: UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16))
         cell.layer.cornerRadius = 10
         cell.layer.borderWidth = 5
@@ -158,8 +205,8 @@ class ShoppingTableViewController: UITableViewController {
         let star = date.star ? "star.fill" : "star"
         let starImage = UIImage(systemName: star)
         cell.starButton.setImage(starImage, for: .normal)
-     
-
+        
+        
         cell.checkButton.tag = indexPath.row
         cell.checkButton.addTarget(self, action:#selector(checkButtonTapped), for: .touchUpInside)
         
